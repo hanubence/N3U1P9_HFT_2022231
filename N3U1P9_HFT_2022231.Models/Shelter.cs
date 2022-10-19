5cc551b1c737ce909ea1a0f99f005fbba4c3d250
hanubence <hanuszkabence@gmail.com>
2022-10-19 12:37:47 +0200

    Added collection fields for animals and workers associated to a given shelter.

--- N3U1P9_HFT_2022231.Models/Shelter.cs
+++ N3U1P9_HFT_2022231.Models/Shelter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,6 +10,9 @@ namespace N3U1P9_HFT_2022231.Models
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int shelterId { get; set; }
 
+        public virtual ICollection<Animal> animals { get; set; }
+        public virtual ICollection<ShelterWorker> workers { get; set; }
+
         [Required, StringLength(40)]
         public string shelterName { get; set; }
 
@@ -16,7 +20,6 @@ namespace N3U1P9_HFT_2022231.Models
         public string shelterAddress { get; set; }
 
         [Required, Range(0,100000000)]
-
         public int annualBudget { get; set; }
     }
 }
