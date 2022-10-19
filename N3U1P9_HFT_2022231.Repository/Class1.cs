968a1a566528775e98f87d0f1987397a5406d56b
hanubence <hanuszkabence@gmail.com>
2022-10-19 12:52:28 +0200

    Created Database context for the application

--- N3U1P9_HFT_2022231.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace N3U1P9_HFT_2022231.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ N3U1P9_HFT_2022231.Repository/ShelterDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+using N3U1P9_HFT_2022231.Models;
+
+namespace N3U1P9_HFT_2022231.Repository
+{
+    public class ShelterDbContext : DbContext
+    {
+        public DbSet<Shelter> Shelters { get; set; }
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<ShelterWorker> Workers { get; set; }
+
+        public ShelterDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\ShelterDB.mdf;Integrated Security=True";
+
+                builder.UseSqlServer(conn);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Shelter>(sh => sh
+                .HasMany<Animal>()
+                .OnDelete(DeleteBehavior.Cascade)
+            );
+        }
+    }
+}
