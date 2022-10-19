d7377be7211e3a9d2677651fe6df8d0a783d5a7d
hanubence <hanuszkabence@gmail.com>
2022-10-19 12:06:28 +0200

    Created animal model class.

---
+++ N3U1P9_HFT_2022231.Models/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace N3U1P9_HFT_2022231.Models
+{
+    public class Animal
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int animalId { get; set; }
+
+        [Required, StringLength(30)]
+        public string animalName { get; set; }
+
+        [Required, Range(1,20)]
+        public int animalAge { get; set; }
+
+        [Required, ForeignKey(nameof(Shelter))]
+        public int shelterId { get; set; }
+    }
+}
--- N3U1P9_HFT_2022231.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace N3U1P9_HFT_2022231.Models
-{
-    public class Class1
-    {
-    }
-}
