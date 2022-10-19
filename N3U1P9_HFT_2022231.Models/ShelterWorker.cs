974dce55b37d30d7d1b05dd836ade4f743aa3d1a
hanubence <hanuszkabence@gmail.com>
2022-10-19 12:16:42 +0200

    Created Shelter Worker model class

---
+++ N3U1P9_HFT_2022231.Models/ShelterWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace N3U1P9_HFT_2022231.Models
+{
+    class ShelterWorker
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int workerId { get; set; }
+
+        [Required, StringLength(30)]
+        public string workerName { get; set; }
+
+        [Required, Range(1, 20)]
+        public int workerAge { get; set; }
+
+        [Required, DataType(nameof(DateTime))]
+        public DateTime startDate;
+
+        [Required, ForeignKey(nameof(Shelter))]
+        public int shelterId { get; set; }
+    }
+}
