7833de124a974aec63fc5f1804cfac5645453651
hanubence <hanuszkabence@gmail.com>
2022-11-15 13:41:08 +0100

    Populated the Repository class with CRUD methods.

--- N3U1P9_HFT_2022231.Repository/Repository.cs
+++ N3U1P9_HFT_2022231.Repository/Repository.cs
@@ -8,6 +8,30 @@ namespace N3U1P9_HFT_2022231.Repository
 {
     public abstract class Repository<T> : IRepository<T> where T : class
     {
+        protected ShelterDbContext ctx;
+        public Repository(ShelterDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
 
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
     }
 }
