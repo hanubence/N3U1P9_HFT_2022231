7f5f4100b40c77a4099677511f1fb81cc8a5fa80
hanubence <hanuszkabence@gmail.com>
2022-11-15 13:36:13 +0100

    Created skeleton of the base Repository class and the IRepository interface

---
+++ N3U1P9_HFT_2022231.Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N3U1P9_HFT_2022231.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
---
+++ N3U1P9_HFT_2022231.Repository/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N3U1P9_HFT_2022231.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+
+    }
+}
