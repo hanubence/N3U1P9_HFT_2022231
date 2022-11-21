e6a4bca36c65978e7ba4c6d6aae5d1e07c54447f
hanubence <hanuszkabence@gmail.com>
2022-11-21 21:35:31 +0100

    Created logic base for handling Shelter data.

---
+++ N3U1P9_HFT_2022231.Logic/ShelterLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using N3U1P9_HFT_2022231.Models;
+using N3U1P9_HFT_2022231.Repository;
+
+namespace N3U1P9_HFT_2022231.Logic
+{
+    public class ShelterLogic : IShelterLogic
+    {
+        IRepository<Shelter> Repository;
+        public ShelterLogic(IRepository<Shelter> repository)
+        {
+            Repository = repository;
+        }
+
+        public void Create(Shelter item)
+        {
+            Repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            Repository.Delete(id);
+        }
+
+        public Shelter Read(int id)
+        {
+            return Repository.Read(id);
+        }
+
+        public IQueryable<Shelter> ReadAll()
+        {
+            return Repository.ReadAll();
+        }
+
+        public void Update(Shelter item)
+        {
+            Repository.Update(item);
+        }
+
+        public double? GetAverageBudget()
+        {
+            return Repository.ReadAll().Average(t => t.AnnualBudget);
+        }
+    }
+}
