58bd5b391190bde27673007e8592cc3f8e254af0
hanubence <hanuszkabence@gmail.com>
2022-11-23 16:34:43 +0100

    Created controller for non-crud methods in shelterlogic. Changed launch open page to the swagger main page.

---
+++ N3U1P9_HFT_2022231.Endpoint/Controllers/StatsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using N3U1P9_HFT_2022231.Logic;
+using System.Collections.Generic;
+
+namespace N3U1P9_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatsController : ControllerBase
+    {
+        IShelterLogic logic;
+        public StatsController(IShelterLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public double? GetAverageBudget()
+        {
+            return this.logic.GetAverageBudget();
+        }
+
+        [HttpGet]
+        public IEnumerable<ShelterSalaryInfo> GetAverageSalaryByShelter()
+        {
+            return this.logic.GetAverageSalaryByShelter();
+        }
+
+        [HttpGet]
+        public IEnumerable<WorkerOccupationInfo> GetWorkerOccupationCountByShelter()
+        {
+            return this.logic.GetWorkerOccupationCountByShelter();
+        }
+
+        [HttpGet]
+        public IEnumerable<AnimalSpecieInfo> GetAnimalSpeciesCountByShelter()
+        {
+            return this.logic.GetAnimalSpeciesCountByShelter();
+        }
+
+        [HttpGet]
+        public IEnumerable<AnimalAgeInfo> GetAverageAnimalAgeByShelter()
+        {
+            return this.logic.GetAverageAnimalAgeByShelter();
+        }
+    }
+}
