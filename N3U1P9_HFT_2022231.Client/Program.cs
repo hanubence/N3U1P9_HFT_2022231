0934d39418c0a197cd2cda01174d04b461a83e34
hanubence <hanuszkabence@gmail.com>
2022-11-23 16:53:45 +0100

    Created skeleton for rest client.

--- N3U1P9_HFT_2022231.Client/Program.cs
+++ N3U1P9_HFT_2022231.Client/Program.cs
@@ -1,19 +1,108 @@
 ﻿using System;
-using N3U1P9_HFT_2022231.Repository;
 using N3U1P9_HFT_2022231.Models;
 using System.Linq;
+using ConsoleTools;
 
 namespace N3U1P9_HFT_2022231.Client
 {
     internal class Program
     {
+        static RestService Rest;
+
+        static void Create(string entity)
+        {
+            switch (entity)
+            {
+                case "Shelter":
+                    break;
+                case "Animal":
+                    break;
+                case "ShelterWorker":
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        static void List(string entity)
+        {
+            switch (entity)
+            {
+                case "Shelter":
+                    break;
+                case "Animal":
+                    break;
+                case "ShelterWorker":
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void Update(string entity)
+        {
+            switch (entity)
+            {
+                case "Shelter":
+                    break;
+                case "Animal":
+                    break;
+                case "ShelterWorker":
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void Delete(string entity)
+        {
+            switch (entity)
+            {
+                case "Shelter":
+                    break;
+                case "Animal":
+                    break;
+                case "ShelterWorker":
+                    break;
+                default:
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
-            ShelterDbContext context = new ShelterDbContext();
+            Rest = new RestService("http://localhost:45007", "shelter");
 
-            var allatok = context.Animals.AsQueryable();
+            var ShelterSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Shelter"))
+                .Add("Create", () => Create("Shelter"))
+                .Add("Delete", () => Delete("Shelter"))
+                .Add("Update", () => Update("Shelter"))
+                .Add("Exit", ConsoleMenu.Close);
 
+            var AnimalSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Animal"))
+                .Add("Create", () => Create("Animal"))
+                .Add("Delete", () => Delete("Animal"))
+                .Add("Update", () => Update("Animal"))
+                .Add("Exit", ConsoleMenu.Close);
 
+            var ShelterWorkerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("ShelterWorker"))
+                .Add("Create", () => Create("ShelterWorker"))
+                .Add("Delete", () => Delete("ShelterWorker"))
+                .Add("Update", () => Update("ShelterWorker"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Shelters", () => ShelterSubMenu.Show())
+                .Add("Animals", () => AnimalSubMenu.Show())
+                .Add("Shelter Workers", () => ShelterWorkerSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
+
+            Console.ReadLine();
             ;
         }
     }
---
+++ N3U1P9_HFT_2022231.Client/RestService.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N3U1P9_HFT_2022231.Client
+{
+    class RestService
+    {
+        HttpClient client;
+
+        public RestService(string baseurl, string pingableEndpoint = "swagger")
+        {
+            bool isOk = false;
+            do
+            {
+                isOk = Ping(baseurl + pingableEndpoint);
+            } while (isOk == false);
+            Init(baseurl);
+        }
+
+        private bool Ping(string url)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadData(url);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void Init(string baseurl)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseurl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue
+                ("application/json"));
+            try
+            {
+                client.GetAsync("").GetAwaiter().GetResult();
+            }
+            catch (HttpRequestException)
+            {
+                throw new ArgumentException("Endpoint is not available!");
+            }
+
+        }
+
+        public List<T> Get<T>(string endpoint)
+        {
+            List<T> items = new List<T>();
+            HttpResponseMessage response = client.GetAsync(endpoint).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                items = response.Content.ReadAsAsync<List<T>>().GetAwaiter().GetResult();
+            }
+            else
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
+            return items;
+        }
+
+        public T GetSingle<T>(string endpoint)
+        {
+            T item = default(T);
+            HttpResponseMessage response = client.GetAsync(endpoint).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                item = response.Content.ReadAsAsync<T>().GetAwaiter().GetResult();
+            }
+            else
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
+            return item;
+        }
+
+        public T Get<T>(int id, string endpoint)
+        {
+            T item = default(T);
+            HttpResponseMessage response = client.GetAsync(endpoint + "/" + id.ToString()).GetAwaiter().GetResult();
+            if (response.IsSuccessStatusCode)
+            {
+                item = response.Content.ReadAsAsync<T>().GetAwaiter().GetResult();
+            }
+            else
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
+            return item;
+        }
+
+        public void Post<T>(T item, string endpoint)
+        {
+            HttpResponseMessage response =
+                client.PostAsJsonAsync(endpoint, item).GetAwaiter().GetResult();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
+            response.EnsureSuccessStatusCode();
+        }
+
+        public void Delete(int id, string endpoint)
+        {
+            HttpResponseMessage response =
+                client.DeleteAsync(endpoint + "/" + id.ToString()).GetAwaiter().GetResult();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        public void Put<T>(T item, string endpoint)
+        {
+            HttpResponseMessage response =
+                client.PutAsJsonAsync(endpoint, item).GetAwaiter().GetResult();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = response.Content.ReadAsAsync<RestExceptionInfo>().GetAwaiter().GetResult();
+                throw new ArgumentException(error.Msg);
+            }
+
+            response.EnsureSuccessStatusCode();
+        }
+
+    }
+    public class RestExceptionInfo
+    {
+        public RestExceptionInfo()
+        {
+
+        }
+        public string Msg { get; set; }
+    }
+}
