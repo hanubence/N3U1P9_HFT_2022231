dda98b699b680ab46398f6050592bbe547a6a347
hanubence <hanuszkabence@gmail.com>
2022-11-23 19:24:41 +0100

    Implemented Create method on the client side for all model types.

--- N3U1P9_HFT_2022231.Client/Program.cs
+++ N3U1P9_HFT_2022231.Client/Program.cs
@@ -2,6 +2,7 @@
 using N3U1P9_HFT_2022231.Models;
 using System.Linq;
 using ConsoleTools;
+using System.Collections.Generic;
 
 namespace N3U1P9_HFT_2022231.Client
 {
@@ -14,12 +15,59 @@ namespace N3U1P9_HFT_2022231.Client
             switch (entity)
             {
                 case "Shelter":
+                    Shelter shelter = new Shelter();
+
+                    Console.WriteLine("[Required] Name of the new shelter: ");
+                    shelter.Name = Console.ReadLine();
+
+                    Console.WriteLine("[Not Required] Address of the new shelter: ");
+                    shelter.Address = Console.ReadLine();
+
+                    Console.WriteLine("[Not Required] Annual budget of the new shelter: ");
+                    shelter.AnnualBudget = NumCheck(Console.ReadLine());
+
+                    Rest.Post<Shelter>(shelter, "Shelter");
                     break;
                 case "Animal":
+                    Animal animal = new Animal();
+
+                    Console.WriteLine("[Required] Name of the new animal: ");
+                    animal.Name = Console.ReadLine();
+
+                    Console.WriteLine("[Required] ID of the shelter where the new animal lives at: ");
+                    animal.ShelterId = NumCheck(Console.ReadLine());
+
+                    Console.WriteLine("[Not Required] Age of the new animal: ");
+                    animal.Age = NumCheck(Console.ReadLine());
+
+                    Console.WriteLine("[Not Required] Species of the new animal: ");
+                    animal.Species = Console.ReadLine();
+
                     break;
                 case "ShelterWorker":
-                    break;
-                default:
+                    ShelterWorker worker = new ShelterWorker();
+
+                    Console.WriteLine("[Required] Name of the new worker: ");
+                    worker.Name = Console.ReadLine();
+
+                    Console.WriteLine("[Required] ID of the shelter where the new worker works at: ");
+                    worker.ShelterId = NumCheck(Console.ReadLine());
+
+                    Console.WriteLine("[Not Required] Age of the new worker: ");
+                    worker.Age = NumCheck(Console.ReadLine());
+
+                    Console.WriteLine("[Not Required] Salary of the new worker: ");
+                    worker.Salary = NumCheck(Console.ReadLine());
+
+                    Console.WriteLine("[Not Required] Occupation of the new worker: ");
+                    worker.Occupation = Console.ReadLine();
+
+                    Console.WriteLine("[Not Required] Hire data of the new worker: (Empty = Now)");
+                    DateTime date;
+                    if (DateTime.TryParse(Console.ReadLine(), out date)) worker.HireDate = date;
+                    else worker.HireDate = DateTime.Now;
+
+                    Console.WriteLine();
                     break;
             }
         }
@@ -29,14 +77,30 @@ namespace N3U1P9_HFT_2022231.Client
             switch (entity)
             {
                 case "Shelter":
+                    List<Shelter> shelters = Rest.Get<Shelter>("Shelter");
+                    foreach (Shelter shelter in shelters)
+                    {
+                        Console.WriteLine($"{shelter.ShelterId}: {shelter.Name} ({shelter.Address})");
+                    }
                     break;
                 case "Animal":
+                    List<Animal> animals = Rest.Get<Animal>("Animal");
+                    foreach (var animal in animals)
+                    {
+                        Console.WriteLine($"{animal.AnimalId}: {animal.Name} ({animal.Age}yr / {animal.Species})");
+                    }
                     break;
                 case "ShelterWorker":
+                    List<ShelterWorker> workers = Rest.Get<ShelterWorker>("ShelterWorker");
+                    foreach (ShelterWorker worker in workers)
+                    {
+                        Console.WriteLine($"{worker.WorkerId}: {worker.Name} [{worker.Occupation}]");
+                    }
                     break;
                 default:
                     break;
             }
+            Console.ReadLine();
         }
 
         static void Update(string entity)
@@ -56,22 +120,40 @@ namespace N3U1P9_HFT_2022231.Client
 
         static void Delete(string entity)
         {
+            int id;
             switch (entity)
             {
                 case "Shelter":
+                    Console.WriteLine("ID to delete:");
+                    id = int.Parse(Console.ReadLine());
+                    Rest.Delete(id, "Shelter");
                     break;
                 case "Animal":
+                    Console.WriteLine("ID to delete:");
+                    id = int.Parse(Console.ReadLine());
+                    Rest.Delete(id, "Animal");
                     break;
                 case "ShelterWorker":
+                    Console.WriteLine("ID to delete:");
+                    id = int.Parse(Console.ReadLine());
+                    Rest.Delete(id, "ShelterWorker");
                     break;
                 default:
                     break;
             }
         }
 
+        static int NumCheck(string n)
+        {
+            int number;
+            bool isNumber = int.TryParse(n, out number);
+
+            return isNumber ? number : 0;
+        }
+
         static void Main(string[] args)
         {
-            Rest = new RestService("http://localhost:45007", "shelter");
+            Rest = new RestService("http://localhost:45007/");
 
             var ShelterSubMenu = new ConsoleMenu(args, level: 1)
                 .Add("List", () => List("Shelter"))
@@ -101,9 +183,6 @@ namespace N3U1P9_HFT_2022231.Client
                 .Add("Exit", ConsoleMenu.Close);
 
             menu.Show();
-
-            Console.ReadLine();
-            ;
         }
     }
 }
