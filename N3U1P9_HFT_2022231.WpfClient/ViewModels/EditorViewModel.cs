6c72ca53a3f083e13921097e51ada0866edb2f31
hanubence <hanuszkabence@gmail.com>
2023-05-13 15:53:35 +0200

    [ADDED] Editor viewmodel

---
+++ N3U1P9_HFT_2022231.WpfClient/ViewModels/EditorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace N3U1P9_HFT_2022231.WpfClient.ViewModels
+{
+    public class EditorViewModel<T>
+    {
+        public T Actual { get; set; }
+        public ICommand CloseEditorCommand { get; set; }
+        public void Setup(T actual) { Actual = actual; }
+        public EditorViewModel()
+        {
+
+        }
+    }
+}
