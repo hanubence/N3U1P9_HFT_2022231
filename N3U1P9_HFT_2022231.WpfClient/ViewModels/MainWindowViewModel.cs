44b26495c64825b7878c885da353d84709500852
hanubence <hanuszkabence@gmail.com>
2023-05-13 14:07:58 +0200

    [ADDED] Created main window viewmodel and added RestCollection helper class

---
+++ N3U1P9_HFT_2022231.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace N3U1P9_HFT_2022231.WpfClient.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public RestColl
+    }
+}
