61026ebe002440c0f3ec10ec071892e56be869f3
hanubence <hanuszkabence@gmail.com>
2023-05-17 21:27:19 +0200

    Progress in the JSClient

--- N3U1P9_HFT_2022231.JSClient/wwwroot/index.js
+++ N3U1P9_HFT_2022231.JSClient/wwwroot/index.js
@@ -1,3 +1,89 @@
-let data = fetch('http://localhost:5000/Shelter').
-    then(x => x.json()).
-    then(x => console.log(x))
+let shelters = [];
+let connection = null;
+getdata();
+setupSignalR();
+
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:5000/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("ShelterCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("ShelterDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+
+
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+async function getdata() {
+    await fetch('http://localhost:5000/Shelter')
+        .then(x => x.json())
+        .then(y => {
+            shelters = y;
+            display();
+        });
+}
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    shelters.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.shelterId + "</td><td>"
+            + t.name + "</td><td>" +
+            `<button type="button" onclick="remove(${t.shelterId})">Delete</button>`
+            + "</td></tr>";
+    });
+}
+
+function remove(id) {
+    fetch('http://localhost:5000/Shelter/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function create() {
+    let name = document.getElementById('sheltername').value;
+    fetch('http://localhost:5000/Shelter', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { Name: name })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
